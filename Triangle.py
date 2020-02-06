5d0f70bc79c48ad1b5d80c1eddc414b6791f10de
jasperP <53663744+jasperpwj@users.noreply.github.com>
2020-02-05 22:59:22 -0500

    Modified function file

--- Triangle.py
+++ Triangle.py
@@ -9,6 +9,7 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
+
 def classifyTriangle(a,b,c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
@@ -26,32 +27,34 @@ def classifyTriangle(a,b,c):
       
       BEWARE: there may be a bug or two in this code
     """
+    # verify that all 3 inputs are integers
+    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    if not (isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
+        return 'InvalidInput'
 
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
-    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-      
+
+
+    num = [a, b, c]
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a:
+    if a == b and b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif a == b or a == c or b == c:
+        return 'Isosceles'
+    elif (max(num))**2 == (min(num))**2 + (sum(num) - max(num) - min(num))**2:
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
     else:
-        return 'Isoceles'
+        return 'Scalene'
+
